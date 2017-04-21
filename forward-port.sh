e8a371d13f6d1152c20c24d720397ee0e2a68fda
Siêu <SieuNT@users.noreply.github.com>
2017-04-21 13:25:12 +0700

    Delete forward-port.sh

--- forward-port.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-iptables -F
-iptables -t nat -F
-iptables -X
-
-iptables -t nat -A PREROUTING -p tcp --dport $2 -j DNAT --to-destination $1:$2
-iptables -t nat -A POSTROUTING -p tcp -d $1 --dport $2 -j SNAT --to-source $1
