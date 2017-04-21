b6d16e390d31ba8a90217dac66b4202b4ffbf1a3
Siêu <SieuNT@users.noreply.github.com>
2017-04-21 13:25:25 +0700

    Add files via upload

---
+++ fwport.sh
@@ -0,0 +1,12 @@
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination 172.17.0.2
+iptables -t nat -A POSTROUTING -o eth0 -p tcp --dport 80 -j SNAT --to-source 172.17.0.2
